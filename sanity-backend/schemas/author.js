c9437e8086bfb0a5b884271c40f2cbe814d6ceae
Muhammad Huzaifa <mdhuzaifapatel@gmail.com>
2022-12-05 02:10:14 +0530

    changes

---
+++ sanity-backend/schemas/author.js
@@ -0,0 +1,26 @@
+export default {
+  name: "author",
+  type: "document",
+  title: "Author",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title",
+    },
+
+    {
+      name: "about",
+      type: "string",
+      title: "About",
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
